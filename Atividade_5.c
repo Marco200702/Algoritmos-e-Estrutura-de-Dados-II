7a874c0d07df44594d15adcbd01226ef481d631a
Marco200702 <mcordeirodegoes@gmail.com>
2025-10-01 22:32:23 -0300

    01/10/2025

--- Atividade_5.c
+++ Atividade_5.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-
 /*01 - Criar um programa que permita o usuário informar o modelo, a
 marca e o ano do carro. Pegue esses dados informados e
 guarde em uma estrutura de dados.
@@ -27,15 +23,76 @@ em decibéis;
 o ruído é baixo (< BAIXO), médio (entre BAIXO e MEDIO), alto
 (entre MEDIO e ALTO) ou muito alto*/
 
-typedef struct{
-    char marca[50];
-    char modelo[50];
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct 
+{
+    char modelo[51];
+    char marca[51];
     int ano;
     bool transferido;
+
 } carro;
 
 
+int main(void) {
+    carro car[2];
+    int tamVetor = sizeof(car) / sizeof(car[0]);
+    int resultado_tranferido = 0;
 
+    for (int i = 0; i < tamVetor; i++){   
+        for (int j = 0; j < sizeof(car[i].marca); j++) 
+        {
+            car[i].marca[j] = '\0';
+            car[i].modelo[j] = '\0';
+        }
+        car[i].ano = 0;
+        car[i].transferido = false;
+    }
 
+    int i = 0;
+    while (i < tamVetor) 
+    {
+        printf("\nInforme o modelo: ");
+        fgets(car[i].modelo, sizeof(car[i].modelo), stdin);
+        
+        printf("Informe a marca: ");
+        fgets(car[i].marca, sizeof(car[i].marca), stdin);
+        
+        printf("Informe o ano: ");
+        scanf("%i", &car[i].ano);
 
         
+        printf("Insira se foi transferido (1 para sim e 0 para não): ");
+        scanf("%d", &resultado_tranferido);
+        if(resultado_tranferido == 1){
+            car[i].transferido = true;
+        }
+        else if(resultado_tranferido == 0){
+            car[i].transferido = false;
+        }
+        else{
+            printf("O valor inserido não eh correspondente!");
+        }
+    
+        getchar();
+        getchar();
+        i++;
+    }
+
+   
+        
+        printf("\n### Carro %d ###\n", i + 1);
+        printf(" Modelo: %s\r Marca: %s\r Ano: %i\r Transferido: %i", 
+                car[i].modelo, 
+                car[i].marca, 
+                car[i].ano,
+                car[i].transferido);
+        printf("\n###############\n");
+    
+
+    return 0;
+}
